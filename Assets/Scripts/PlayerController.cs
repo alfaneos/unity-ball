9a76334467867627b770534a070113ad213f4338
alfaneos <alfaneos@gmail.com>
2018-02-03 17:45:18 +0600

    1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody rb;
+    public float speed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 v = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(v * speed);
+    }
+
+	void OnTriggerEnter(Collider other) {
+//		Destroy(other.gameObject);
+		if(other.gameObject.CompareTag("Pick Up")){
+			other.gameObject.SetActive (false);
+		}
+	}
+}
